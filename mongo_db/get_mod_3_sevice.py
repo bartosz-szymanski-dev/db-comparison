bb901058ee9ba410c4f22d4d62df83b4ec24180e
Bartek <szymaskibartosz@gmail.com>
2023-02-09 21:20:06 +0100

    [mod3] fix

--- mongo_db/get_mod_3_sevice.py
+++ mongo_db/get_mod_3_sevice.py
@@ -6,8 +6,10 @@ import mongo_db.collection_helper as data_collection_helper
 def run_benchmark(client):
     print('Benchmark name: MongoDB Get every 3rd document')
     data_collection = data_collection_helper.get_data_collection(client)
+    max_number = 112634
+    modulo_3_list = [str(number * 3) for number in range(int(max_number / 3))]
     start = time.time()
-    pymongo_cursor = data_collection.find({}).skip(2).limit(1)
+    pymongo_cursor = data_collection.find({"index": {"$in": modulo_3_list}})
     all_data = list(pymongo_cursor)
     print('Query time: ' + str(round(time.time() - start, 2)) + 's')
     print('Rows fetched: ', str(len(all_data)))
